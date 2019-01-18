c3a5d69b2102b41be79f0351129e436c6cea051a
Elvira <1191757361@qq.com>
2019-01-18 23:24:17 +0800

    搜索条控件自定义实例化

---
+++ app/src/main/java/com/example/my_video/view/TitleBar.java
@@ -0,0 +1,53 @@
+package com.example.my_video.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.example.my_video.R;
+
+public class TitleBar extends LinearLayout implements View.OnClickListener{
+    private Context context;
+    private View search;
+    private View rl_game;
+    private View iv_record;
+    public TitleBar(Context context) {
+        this(context,null);
+    }
+
+    public TitleBar(Context context, AttributeSet attrs) {
+        this(context, attrs,0);
+    }
+
+    public TitleBar(Context context,AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        this.context=context;
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        //实例化孩子
+        search = getChildAt(1);
+        rl_game = getChildAt(2);
+        iv_record = getChildAt(3);
+
+        search.setOnClickListener(this);
+        rl_game.setOnClickListener(this);
+        iv_record.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.search:Toast.makeText(context,"Search",Toast.LENGTH_SHORT).show();
+            break;
+            case R.id.rl_game:Toast.makeText(context,"Game",Toast.LENGTH_SHORT).show();
+            break;
+            case R.id.iv_record:Toast.makeText(context,"Record",Toast.LENGTH_SHORT).show();
+            break;
+        }
+    }
+}
