33e7d9c74cf044c9199b8d76b6e404b07a92eb0d
Elvira <1191757361@qq.com>
2019-01-19 23:44:29 +0800

    加载本地视频数据

---
+++ app/src/main/java/com/example/my_video/dto/VideoItem.java
@@ -0,0 +1,49 @@
+package com.example.my_video.dto;
+
+public class VideoItem {
+    private String name;//视频名称
+    private long duration;//视频时长
+    private long size;//视频大小
+    private String data;//视频路径
+    private String artist;//歌者名
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getDuration() {
+        return duration;
+    }
+
+    public void setDuration(long duration) {
+        this.duration = duration;
+    }
+
+    public long getSize() {
+        return size;
+    }
+
+    public void setSize(long size) {
+        this.size = size;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+}
